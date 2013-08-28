a70b3abe6c75632e846986ddc91a6075d288dbaa
retspen <anatoliy.guskov@gmail.com>
2013-08-28 16:11:33 +0300

    Add jenkins

--- webvirtmgr/settings.py
+++ webvirtmgr/settings.py
@@ -78,11 +78,30 @@ INSTALLED_APPS = (
 )
 
 INSTALLED_APPS += ('django_jenkins',)
-JENKINS_TASKS = ('django_jenkins.tasks.run_pylint',
-                 'django_jenkins.tasks.run_pep8',
-                 'django_jenkins.tasks.run_pyflakes',
-                 'django_jenkins.tasks.with_coverage',
-                 'django_jenkins.tasks.django_tests',)
+
+JENKINS_TASKS = (
+    'django_jenkins.tasks.with_coverage',
+    'django_jenkins.tasks.django_tests',
+    'django_jenkins.tasks.dir_tests',
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_pyflakes',
+    # 'django_jenkins.tasks.run_jslint',
+    # 'django_jenkins.tasks.run_csslint',    
+    # 'django_jenkins.tasks.run_sloccount',    
+    # 'django_jenkins.tasks.lettuce_tests',
+)
+
+PROJECT_APPS = (
+    'dashboard',
+    'overview',
+    'newvm',
+    'storage',
+    'network',
+    'snapshot',
+    'vds',
+    'vnc',
+    'webvirtmgr',
+)
 
 LOGGING = {
     'version': 1,
