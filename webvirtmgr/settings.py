b1e0e677d17135ca902f0ca03ae79842e9dddbf6
retspen <anatoliy.guskov@gmail.com>
2013-08-28 17:22:30 +0300

    add pylint

--- webvirtmgr/settings.py
+++ webvirtmgr/settings.py
@@ -80,9 +80,9 @@ INSTALLED_APPS = (
 INSTALLED_APPS += ('django_jenkins',)
 
 JENKINS_TASKS = (
+    'django_jenkins.tasks.run_pylint',
     'django_jenkins.tasks.with_coverage',
     'django_jenkins.tasks.django_tests',
-    'django_jenkins.tasks.dir_tests',
     'django_jenkins.tasks.run_pep8',
     'django_jenkins.tasks.run_pyflakes',
     # 'django_jenkins.tasks.run_jslint',
