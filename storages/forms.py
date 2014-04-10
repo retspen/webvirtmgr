3fa2025c6140b9aeae6774297d0031c1f5c2e50d
Simon Körner <github@lubiland.de>
2014-04-10 16:01:20 +0200

    recommendation for qcow2

--- storages/forms.py
+++ storages/forms.py
@@ -48,7 +48,7 @@ class AddStgPool(forms.Form):
 
 class AddImage(forms.Form):
     name = forms.CharField(max_length=20)
-    format = forms.ChoiceField(required=True, choices=(('raw', 'raw'), ('qcow', 'qcow'), ('qcow2', 'qcow2')))
+    format = forms.ChoiceField(required=True, choices=(('raw', 'raw'), ('qcow', 'qcow'), ('qcow2', 'qcow2 (recommended due to snapshots and thin privisioning)')))
     size = forms.IntegerField()
 
     def clean_name(self):
@@ -65,7 +65,7 @@ class CloneImage(forms.Form):
     name = forms.CharField(max_length=20)
     image = forms.CharField(max_length=20)
     convert = forms.BooleanField(required=False)
-    format = forms.ChoiceField(required=False, choices=(('qcow2', 'qcow2'), ('qcow', 'qcow'), ('raw', 'raw')))
+    format = forms.ChoiceField(required=False, choices=(('qcow2', 'qcow2 (recommended due to snapshots and thin privisioning)'), ('qcow', 'qcow'), ('raw', 'raw')))
 
     def clean_name(self):
         name = self.cleaned_data['name']
