d368dc9f0b20c9eb39f143fd698a21ea631924da
Daniele Viganò <daniele@vigano.me>
2014-05-27 12:02:36 +0200

    Added missing admin.py in instance

---
+++ instance/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
+from models import Instance
+
+class InstanceAdmin(ModelAdmin):
+    pass
+
+admin.site.register(Instance, InstanceAdmin)
