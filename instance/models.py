cc4fde8763248b84bc2d1967a504ac9b49872613
Daniele Viganò <daniele@vigano.me>
2014-05-27 08:09:44 +0200

    Fix a typo

--- instance/models.py
+++ instance/models.py
@@ -7,7 +7,7 @@ class Instance(models.Model):
     compute = models.ForeignKey(Compute)
     name = models.CharField(max_length=20)
     uuid = models.CharField(max_length=36)
-    acl = models.ManyToManyField(User, related_name='istance')
+    acl = models.ManyToManyField(User, related_name='instance')
 #    display_name = models.CharField(max_length=50)
 #    display_description = models.CharField(max_length=255)
 
