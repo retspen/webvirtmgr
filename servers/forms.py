b2104bbb09496362b8d3e06558c5c21b43df477d
Daniele Viganò <daniele@vigano.me>
2014-10-25 14:24:25 +0200

    Add support for servers with same name if hyper is different

--- servers/forms.py
+++ servers/forms.py
@@ -20,19 +20,21 @@ class ComputeAddTcpForm(forms.Form):
 
     def clean_name(self):
         name = self.cleaned_data['name']
+        hypervisor = self.data['hypervisor']
         have_symbol = re.match('[^a-zA-Z0-9._-]+', name)
         if have_symbol:
             raise forms.ValidationError(_('The host name must not contain any special characters'))
         elif len(name) > 20:
             raise forms.ValidationError(_('The host name must not exceed 20 characters'))
         try:
-            Compute.objects.get(name=name)
+            Compute.objects.get(name=name, hypervisor=hypervisor)
         except Compute.DoesNotExist:
             return name
         raise forms.ValidationError(_('This host is already connected'))
 
     def clean_hostname(self):
         hostname = self.cleaned_data['hostname']
+        hypervisor = self.data['hypervisor']
         have_symbol = re.match('[^a-z0-9.-]+', hostname)
         wrong_ip = re.match('^0.|^255.', hostname)
         if have_symbol:
@@ -40,7 +42,7 @@ class ComputeAddTcpForm(forms.Form):
         elif wrong_ip:
             raise forms.ValidationError(_('Wrong IP address'))
         try:
-            Compute.objects.get(hostname=hostname)
+            Compute.objects.get(hostname=hostname, hypervisor=hypervisor)
         except Compute.DoesNotExist:
             return hostname
         raise forms.ValidationError(_('This host is already connected'))
@@ -58,19 +60,21 @@ class ComputeAddSshForm(forms.Form):
 
     def clean_name(self):
         name = self.cleaned_data['name']
+        hypervisor = self.data['hypervisor']
         have_symbol = re.match('[^a-zA-Z0-9._-]+', name)
         if have_symbol:
             raise forms.ValidationError(_('The name of the host must not contain any special characters'))
         elif len(name) > 20:
             raise forms.ValidationError(_('The name of the host must not exceed 20 characters'))
         try:
-            Compute.objects.get(name=name)
+            Compute.objects.get(name=name, hypervisor=hypervisor)
         except Compute.DoesNotExist:
             return name
         raise forms.ValidationError(_('This host is already connected'))
 
     def clean_hostname(self):
         hostname = self.cleaned_data['hostname']
+        hypervisor = self.data['hypervisor']
         have_symbol = re.match('[^a-zA-Z0-9._-]+', hostname)
         wrong_ip = re.match('^0.|^255.', hostname)
         if have_symbol:
@@ -78,7 +82,7 @@ class ComputeAddSshForm(forms.Form):
         elif wrong_ip:
             raise forms.ValidationError(_('Wrong IP address'))
         try:
-            Compute.objects.get(hostname=hostname)
+            Compute.objects.get(hostname=hostname, hypervisor=hypervisor)
         except Compute.DoesNotExist:
             return hostname
         raise forms.ValidationError(_('This host is already connected'))
@@ -98,19 +102,21 @@ class ComputeAddTlsForm(forms.Form):
 
     def clean_name(self):
         name = self.cleaned_data['name']
+        hypervisor = self.data['hypervisor']
         have_symbol = re.match('[^a-zA-Z0-9._-]+', name)
         if have_symbol:
             raise forms.ValidationError(_('The host name must not contain any special characters'))
         elif len(name) > 20:
             raise forms.ValidationError(_('The host name must not exceed 20 characters'))
         try:
-            Compute.objects.get(name=name)
+            Compute.objects.get(name=name, hypervisor=hypervisor)
         except Compute.DoesNotExist:
             return name
         raise forms.ValidationError(_('This host is already connected'))
 
     def clean_hostname(self):
         hostname = self.cleaned_data['hostname']
+        hypervisor = self.data['hypervisor']
         have_symbol = re.match('[^a-z0-9.-]+', hostname)
         wrong_ip = re.match('^0.|^255.', hostname)
         if have_symbol:
@@ -118,7 +124,7 @@ class ComputeAddTlsForm(forms.Form):
         elif wrong_ip:
             raise forms.ValidationError(_('Wrong IP address'))
         try:
-            Compute.objects.get(hostname=hostname)
+            Compute.objects.get(hostname=hostname, hypervisor=hypervisor)
         except Compute.DoesNotExist:
             return hostname
         raise forms.ValidationError(_('This host is already connected'))
@@ -164,13 +170,14 @@ class ComputeAddSocketForm(forms.Form):
 
     def clean_name(self):
         name = self.cleaned_data['name']
+        hypervisor = self.data['hypervisor']
         have_symbol = re.match('[^a-zA-Z0-9._-]+', name)
         if have_symbol:
             raise forms.ValidationError(_('The host name must not contain any special characters'))
         elif len(name) > 20:
             raise forms.ValidationError(_('The host name must not exceed 20 characters'))
         try:
-            Compute.objects.get(name=name)
+            Compute.objects.get(name=name, hypervisor=hypervisor)
         except Compute.DoesNotExist:
             return name
         raise forms.ValidationError(_('This host is already connected'))
