4dde2e0443315af7cae0c7295e62536109c485e2
Finy <jos666@qq.com>
2017-06-23 10:21:34 +0800

    To solve the new virtual machine host name can not exceed 14 characters file problem, the current modified host name 50 characters

--- create/models.py
+++ create/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Flavor(models.Model):
-    label = models.CharField(max_length=12)
+    label = models.CharField(max_length=50)
     memory = models.IntegerField()
     vcpu = models.IntegerField()
     disk = models.IntegerField()
