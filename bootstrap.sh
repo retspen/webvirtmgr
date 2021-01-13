aeafc2115221a5e02408ff873c9727f6e10bfca1
chenjiajun <chenjiajjun@cestc.cn>
2021-01-13 09:48:26 +0800

    1.request.txt update for django

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,9 @@
 if [ ! -f "/data/vm/webvirtmgr.sqlite3" ]; then
 
 chown webvirtmgr /var/run/libvirt/libvirt-sock
-/usr/bin/python /opt/webvirtmgr/manage.py syncdb --noinput
+#/usr/bin/python /opt/webvirtmgr/manage.py syncdb --noinput
+/usr/bin/python /opt/webvirtmgr/manage.py makemigrations
+/usr/bin/python /opt/webvirtmgr/manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@localhost', 'Cestc_01')" | /usr/bin/python /opt/webvirtmgr/manage.py shell
 
 fi
