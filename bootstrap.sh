1c8492e7ae5b62b27c450d8d4cbf4432ba3f4011
chenjiajun <chenjiajjun@cestc.cn>
2021-01-22 18:18:50 +0800

    1.update docker build for v5.0.0

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,11 @@ chown webvirtmgr /var/run/libvirt/libvirt-sock
 /usr/bin/python /opt/webvirtmgr/manage.py makemigrations
 /usr/bin/python /opt/webvirtmgr/manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@localhost', 'Cestc_01')" | /usr/bin/python /opt/webvirtmgr/manage.py shell
+chown -R webvirtmgr:libvirtd /data/vm
+su - webvirtmgr -s /bin/bash
+ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
+touch ~/.ssh/config && echo -e "StrictHostKeyChecking=no\nUserKnownHostsFile=/dev/null" >> ~/.ssh/config
+chmod 0600 ~/.ssh/config
 
 fi
 
