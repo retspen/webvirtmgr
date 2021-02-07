635c9e4ee81a3c991bc95a68bf2a388eebbb96dc
chenjiajun <chenjiajjun@cestc.cn>
2021-02-07 18:26:06 +0800

    1.add rbac app 2.update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,7 @@ if [ ! -f "/data/vm/webvirtmgr.sqlite3" ]; then
 
 chown webvirtmgr /var/run/libvirt/libvirt-sock
 #/usr/bin/python /opt/webvirtmgr/manage.py syncdb --noinput
-/usr/bin/python /opt/webvirtmgr/manage.py makemigrations
-/usr/bin/python /opt/webvirtmgr/manage.py migrate
+/usr/bin/python /opt/webvirtmgr/manage.py makemigrations && /usr/bin/python /opt/webvirtmgr/manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@localhost', 'Cestc_01')" | /usr/bin/python /opt/webvirtmgr/manage.py shell
 chown -R webvirtmgr:libvirtd /data/vm
 su - webvirtmgr -s /bin/bash
