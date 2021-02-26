2c65195132b085647852b213d1d1a4f881bd1901
chenjiajun <chenjiajjun@cestc.cn>
2021-02-26 17:32:52 +0800

    1.update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ if [ ! -f "/data/vm/webvirtmgr.sqlite3" ]; then
 /usr/bin/python /opt/webvirtmgr/manage.py makemigrations && /usr/bin/python /opt/webvirtmgr/manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@localhost', 'Cestc_01')" | /usr/bin/python /opt/webvirtmgr/manage.py shell
 chown -R webvirtmgr:libvirtd /data/vm
-su - webvirtmgr -c "ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa && touch ~/.ssh/config && echo -e "StrictHostKeyChecking=no\nUserKnownHostsFile=/dev/null" >> ~/.ssh/config && chmod 0600 ~/.ssh/config"
+ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa && touch ~/.ssh/config && echo -e "StrictHostKeyChecking=no\nUserKnownHostsFile=/dev/null" >> ~/.ssh/config && chmod 0600 ~/.ssh/config
 
 fi
 
