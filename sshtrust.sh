17e387805b4bf0a2f561139f22faa818ca32c503
chenjiajun <chenjiajjun@cestc.cn>
2021-03-09 13:55:41 +0800

    1.fix ssh trust script bug

--- sshtrust.sh
+++ sshtrust.sh
@@ -7,15 +7,19 @@ fi
 address=$1
 passwd=$2
 account=$3
-if [ -d "/data/vm/.ssh" ]; then
-    /bin/ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
+id
+if [ ! -d "/data/vm/.ssh" ]; then
+  /bin/ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
+else
+  echo "ssh dir exists!"
 fi
 if [ ! -f "/data/vm/.ssh/config" ]; then
     touch /data/vm/.ssh/config
     echo -e "StrictHostKeyChecking=no\nUserKnownHostsFile=/dev/null" >> /data/vm/.ssh/config
     chmod 0600 /data/vm/.ssh/config
 fi
-sshpass -p "$passwd" ssh-copy-id "$account"@"$address"
+sshpass -p "$passwd" ssh-copy-id -o StrictHostKeyChecking=no "$account"@"$address" 2>&1
 if [ $? -ne 0 ]; then
     echo "ssh key copy error"
+    echo "sshpass -p $passwd ssh-copy-id -o StrictHostKeyChecking=no $account@$address"
 fi
